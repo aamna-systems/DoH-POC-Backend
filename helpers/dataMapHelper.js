e42dd1bf0ba0a4b0e3936ef51ea49fa884cea85b
Taliha Arif <taliha.arif@systemsltd.com>
2022-11-02 16:39:51 +0500

    ..

--- helpers/dataMapHelper.js
+++ helpers/dataMapHelper.js
@@ -230,7 +230,7 @@ function patientEntry(requestBody) {
     return patientData
 }
 
-
+const nodemailer = require("nodemailer");
 const getEmailQuery = (() => {
 
     var d = new Date();
@@ -273,7 +273,38 @@ const getEmailQuery = (() => {
 
         ])
         console.log('results', JSON.stringify(results))
+
+
+
+        //let testAccount = await nodemailer.createTestAccount();
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+            user: 'clarissa.price@ethereal.email', // generated ethereal user
+            pass: 'CHWTPmczzbuG1MxUBU', // generated ethereal password
+            },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Fred Foo 👻" jewino2698@keshitv.com', // sender address
+            to: "jewino2698@keshitv.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: JSON.stringify(results), // plain text body
+            html: "<b>Hello world?</b>", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     });
+
 })
 
 
